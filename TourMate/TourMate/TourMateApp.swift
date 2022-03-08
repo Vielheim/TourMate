26c14f3c4d9a11103fc986483096d678f0b7ec1a
Terence Ho <>
2022-03-08 23:55:07 +0800

    Add Launch View without functionalities

--- TourMate/TourMate/TourMateApp.swift
+++ TourMate/TourMate/TourMateApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct TourMateApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            LaunchView()
         }
     }
 }
